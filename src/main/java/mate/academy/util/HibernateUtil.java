953fa60a2b2201402aef99ce084d78fc40aadb6c
Oleksii Petrychenko <apetrychenkoff@gmail.com>
2025-08-06 21:49:27 +0300

    fixed a movie service app a bit to make it consistent

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -6,6 +6,9 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static final SessionFactory instance = initSessionFactory();
 
+    public HibernateUtil() {
+    }
+
     private static SessionFactory initSessionFactory() {
         return new Configuration().configure().buildSessionFactory();
     }
